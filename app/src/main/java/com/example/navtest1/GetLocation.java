11a468c245452007171431b199d14a2fbc8cab06
ptes77 <31578309+ptes77@users.noreply.github.com>
2019-02-02 15:33:27 -0800

    temp commit

---
+++ app/src/main/java/com/example/navtest1/GetLocation.java
@@ -0,0 +1,7 @@
+package com.example.navtest1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.NavigationView;
+
+public class GetLocation extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+}
