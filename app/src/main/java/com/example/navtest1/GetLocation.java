00c8f638e5f8aec32eaafcba23333091cdcc89e2
ptes77 <31578309+ptes77@users.noreply.github.com>
2019-02-02 17:22:31 -0800

    Added the first phase of the Location/getLocation class

--- app/src/main/java/com/example/navtest1/GetLocation.java
+++ app/src/main/java/com/example/navtest1/GetLocation.java
@@ -11,6 +11,7 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -19,38 +20,104 @@ import com.google.android.gms.tasks.OnSuccessListener;
 
 public class GetLocation extends Activity {
         private FusedLocationProviderClient mFusedLocationClient;
+        private final Context mContext;
         private LocationManager mLocationManager;
         private LocationListener mLocationListener;
 
         private boolean flag = false;
         private boolean isGPSEnabled = false;
         private boolean isNetworkEnabled = false;
+        private boolean canGetLocation = false;
 
         private Location location;
         private double latitude;
         private double longitude;
-        private static final String TAG = "Debug";
 
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        private static final long MIN_DISTANCE_CHANGE = 10; // 10 meters
+        private static final long MIN_TIME_BETWEEN_UPDATES = 1000 * 15; // 15 seconds
 
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        try {
-            mFusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                @Override
-                public void onSuccess(Location location) {
-                    if (location != null) {
-                        // What will we do with this location
-                        // Use the location to find the public facilities that are near this location
-                    }
-                }
-            });
-        } catch (Exception e) {};
+    public GetLocation(Context context) {
+        this.mContext = context;
+        this.location = getLocation();
     }
 
-    public void onClick(View v) {
-        flag = displayGpsStatus();
+    public Location getLocation() {
+        try {
+            mLocationManager = (LocationManager)mContext.getSystemService(LOCATION_SERVICE);
+            isGPSEnabled = mLocationManager.isProviderEnabled(mLocationManager.GPS_PROVIDER);
+            isNetworkEnabled = mLocationManager.isProviderEnabled(mLocationManager.NETWORK_PROVIDER);
+
+            if (!isGPSEnabled && !isNetworkEnabled) {
+                AlertDialog.Builder dialog = new AlertDialog.Builder(this.mContext);
+                dialog.setMessage("GPS Network Not Available!");
+            } else {
+                this.canGetLocation = true;
+
+                // Find location via network
+                if (isNetworkEnabled) {
+                    mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+                            MIN_TIME_BETWEEN_UPDATES, MIN_DISTANCE_CHANGE, mLocationListener);
+                    Log.d("Network", "Network");
+                    if (mLocationManager != null) {
+                        this.location = mLocationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                        if (location != null) {
+                            latitude = location.getLatitude();
+                            longitude = location.getLongitude();
+                        }
+                    }
+                }
+
+                // Find location via GPS
+                if (isGPSEnabled) {
+                    if (location == null) {
+                        mLocationManager.requestLocationUpdates(
+                                LocationManager.GPS_PROVIDER,
+                                MIN_TIME_BETWEEN_UPDATES,
+                                MIN_DISTANCE_CHANGE, mLocationListener);
+                        Log.d("GPS Enabled", "GPS Enabled");
+                        if (mLocationManager != null) {
+                            this.location = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                            if (location != null) {
+                                latitude = location.getLatitude();
+                                longitude = location.getLongitude();
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return this.location;
+    }
+
+    /*
+    public void stopUsingGPS() {
+        if (this.mLocationManager != null) {
+            this.mLocationManager.removeUpdates(GetLocation.this);
+        }
+    }
+    */
+
+    public double getLatitude() {
+        if (this.location != null) {
+            this.latitude = this.location.getLatitude();
+        }
+
+        return this.latitude;
+    }
+
+    public double getLongitude() {
+        if (this.location != null) {
+            this.longitude = this.location.getLongitude();
+        }
+
+        return this.longitude;
+    }
+
+    public boolean canGetLocation() {
+        return this.canGetLocation;
     }
 
     private boolean isLocationEnabled(Context context) {
@@ -90,6 +157,30 @@ public class GetLocation extends Activity {
         }
         return true;
     }
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        try {
+            mFusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if (location != null) {
+                        // What will we do with this location
+                        // Use the location to find the public facilities that are near this location
+                    }
+                }
+            });
+        } catch (Exception e) {};
+    }
+
+    public void onClick(View v) {
+        flag = displayGpsStatus();
+    }
+
+
     private boolean displayGpsStatus() {
         ContentResolver contentResolver = getBaseContext().getContentResolver();
 
